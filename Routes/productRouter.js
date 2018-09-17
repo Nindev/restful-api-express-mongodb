0d2d9da2c45cc1f63aad39a21b21d894ac23bfe9
DSallee <dsallee.dev@gmail.com>
2018-09-17 15:03:34 +0200

    Refactoring code using a custom middleware

--- Routes/productRouter.js
+++ Routes/productRouter.js
@@ -3,114 +3,65 @@ const Product = require('../models/productModel');
 
 const productRouter = express.Router();
 
-//GET: Display all products 
 productRouter.route('/')
-  .get((req, res) => {
-    Product.find({}, (err, products) => {
-      res.json(products);
-    })
-  })
-
-// POST: Add a product
-productRouter.route('/')
+  // GET: Display all products 
   .get((req, res) => {
     Product.find({}, (err, products) => {
       res.json(products)
     })
   })
+  // POST: Add a product
   .post((req, res) => {
     let product = new Product(req.body);
     product.save();
     res.status(201).send(product)
   })
 
-// PUT: Update product in the database
-productRouter.route('/:productId')
-  .get((req, res) => {
-    Product.findById(req.params.productId, (err, product) => {
-      res.json(product)
-    })
+// MIDDLEWARE
+productRouter.use('/:productId', (req, res, next)=>{
+  Product.findById( req.params.productId, (err, product) => {
+    if(err)
+      res.status(500).send(err)
+    else {
+      req.product = product;
+      next()
+    }  
   })
-  .put((req, res) => {
-    Product.findById(req.params.productId, (err, product) => {
-      product.name = req.body.name;
-      product.sku = req.body.sku;
-      product.brand = req.body.brand;
-      product.description = req.body.description;
-      product.save()
-      res.json(product)
-    })
-  })
+})
 
-// PATCH: Update product properties
 productRouter.route('/:productId')
   .get((req, res) => {
-    Product.findById(req.params.productId, (err, product) => {
-      res.json(product)
-    })
+    res.json(product)
   })
+  // PUT: Update product in the database  
   .put((req, res) => {
-    Product.findById(req.params.productId, (err, product) => {
-      product.name = req.body.name;
-      product.sku = req.body.sku;
-      product.brand = req.body.brand;
-      product.description = req.body.description;
-      product.save()
-      res.json(product)
-    })
+    req.product.name;
+    req.product.sku;
+    req.product.brand;
+    req.product.description;
+    req.product.save()
+    res.json(req.product)
   })
+  // PATCH: Update product properties
   .patch((req, res) => {
-    Product.findById(req.params.productId, (err, product) => {
-      if(req.body._id) {
-        delete req.body._id;
-      }
-      for(let p in req.body){
-        product[p] = req.body[p];
-      }
-      product.save();
-      res.json(product);
-    })
+    if(req.body._id) {
+      delete req.body._id;
+    }
+    for(let p in req.body){
+      req.product[p] = req.body[p];
+    }
+    req.product.save();
+    res.json(req.product);
   })
-
   // DELETE: Remove product from the database
-  productRouter.route('/:productId')
-  .get((req, res) => {
-    Product.findById(req.params.productId, (err, product) => {
-      res.json(product)
-    })
-  })
-  .put((req, res) => {
-    Product.findById(req.params.productId, (err, product) => {
-      product.name = req.body.name;
-      product.sku = req.body.sku;
-      product.brand = req.body.brand;
-      product.description = req.body.description;
-      product.save()
-      res.json(product)
-    })
-  })
-  .patch((req, res) => {
-    Product.findById(req.params.productId, (err, product) => {
-      if(req.body._id) {
-        delete req.body._id;
-      }
-      for(let p in req.body){
-        product[p] = req.body[p];
-      }
-      product.save();
-      res.json(product);
-    })
-  })
   .delete((req, res) => {
-    Product.findById(req.params.productId, (err, product) => {
-      product.remove(err => {
-        if(err) {
-          res.status(500).send(err)
-        }
-        else {
-          res.status(204).send('removed')
-        }
-      })
+    req.product.remove(err => {
+      if(err) {
+        res.status(500).send(err)
+      }
+      else {
+        res.status(204).send('removed')
+      }
     })
   })
 module.exports = productRouter;
